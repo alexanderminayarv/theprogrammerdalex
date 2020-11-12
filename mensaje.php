f8ea3b3f9864359cd249defe6cc9edf83f1b688d
alexanderminayarv <74163120+alexanderminayarv@users.noreply.github.com>
2020-11-12 08:46:29 -0500

    Add files via upload

---
+++ mensaje.php
@@ -0,0 +1,39 @@
+<head>
+    <meta charset="utf-8">
+    <title>Alexander Minaya RV</title>
+    <link href="img/icono1.png" rel="icon">
+    <!-- BOOTSTRAP 4 -->
+    <link rel="stylesheet" href="https://bootswatch.com/4/yeti/bootstrap.min.css">
+</head>
+
+<body style="background-color:#000">
+    <div class="card text-center">
+        <div class="card-body">
+            <h5 class="card-title">Mensaje enviado exitosamente!</h5>
+            <img src="check.png" class="img-fluid" alt="Responsive image"><br><br>
+            <a href="index.php"><button type="submit" class="btn">Regresar</button></a> <br>
+            <img src="mensaje1.png" class="card-img-bottom" alt="" style="width:60%">
+        </div>
+    </div>
+</body>
+<style>
+.btn {
+    color: #6C63FF !important;
+    font-size: 20px;
+    font-weight: 500;
+    padding: 0.5em 1.2em;
+    background: rgba(0, 0, 0, 0);
+    border: 2px solid;
+    border-color: #6C63FF;
+    transition: all 1s ease;
+    position: relative;
+    border-radius: 5px;
+    width: 150px;
+    height: 50px;
+}
+
+.btn:hover {
+    background: #6C63FF;
+    color: #fff !important;
+}
+</style>
